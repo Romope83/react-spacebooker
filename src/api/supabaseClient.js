2d52637cf3108df1cce2965e2ec34e61073dc362
Kaua <kaua.fmelo@hotmail.com>
2025-09-08 19:14:01 -0300

    chave nova

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // As suas credenciais únicas do projeto Supabase.
 // É seguro expor a chave 'anon' publicamente, pois as suas regras de RLS (Row Level Security) protegem os dados.
-const supabaseUrl = 'https://niihozmtaqxfoavobqrr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5paWhvem10YXF4Zm9hdm9icXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NzAzMDQsImV4cCI6MjA3MjM0NjMwNH0.q7izlsp6DVslaaOrlF8ejIOciZA_7XPuuFMhZCzxYFk';
+const supabaseUrl = 'https://hfpfaxjoekbvctqwfsww.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmcGZheGpvZWtidmN0cXdmc3d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNjgyODksImV4cCI6MjA3Mjk0NDI4OX0.HBxvq43GNawzS7rEADPOs-py5PcQWK6nIBZzF2VPfOU';
 
 // Cria e exporta o cliente Supabase, que será usado em toda a aplicação para interagir com o seu backend.
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
